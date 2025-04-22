83bb91711a81d2796dd76809d906a6e69db2b340
Nitin Dhyani <nitindhyani1234@gmail.com>
2025-04-22 22:50:29 +0530

    2nd day

---
+++ Strings.js
@@ -0,0 +1,14 @@
+/*let Value=3
+let NegValue=-Value
+console.log(NegValue)
+*/
+
+let Name = new String("Nitin")
+/*console.log(Name) // print full String
+console.log(Name[1]) //print character at 1st index. index start from 0
+console.log(Name.length) //print length of the string starting from 1
+console.log(Name.toUpperCase()) // print string in uppercase
+console.log(Name.charAt(4)) //Print char at index 4
+*/
+let newString= Name.substring(1,4)
+console.log(newString)
